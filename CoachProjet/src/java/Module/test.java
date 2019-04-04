0fdf601d1c290e7a2e4177ae22386975b53c4cf4
OrangeClement <jiayuchang.tls@gmail.com>
2019-04-04 20:28:46 +0200

    fix pb ordre par alph

--- CoachProjet/src/java/Module/test.java
+++ CoachProjet/src/java/Module/test.java
@@ -5,7 +5,13 @@
  */
 package Module;
 
+import Bd.Client;
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.TreeMap;
 
 /**
  *
@@ -14,6 +20,20 @@ import java.util.HashMap;
 public class test {
      public static void main (String[] args) throws Exception
         {
+            ArrayList<Client> c = HibernateMethode.consultClientPgrm();
+            Map<String,Client> mc= new TreeMap<String, Client>(
+                new Comparator<String>() {
+                    public int compare(String obj1, String obj2) {
+                        // 降序排序
+                        return obj1.compareTo(obj2);
+                    }
+                });
+            for(Client cli : c){
+                mc.put(cli.getNomc().toString(), cli);
+            }
+         for (Map.Entry<String, Client> cc: mc.entrySet()) {
+             System.out.println(cc.getKey());
+         }
 
 
 }
