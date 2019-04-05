f42dfcf5d19c89b8289d63047b32395276a4e0c4
21611943 <21611943@a17717.ad.ut-capitole.fr>
2019-04-05 10:16:08 +0200

    merge with master 04/05/2019

--- CoachProjet/src/java/Module/test.java
+++ CoachProjet/src/java/Module/test.java
@@ -6,6 +6,7 @@
 package Module;
 
 import Bd.Client;
+import Bd.Exerciseperso;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.HashMap;
@@ -20,21 +21,14 @@ import java.util.TreeMap;
 public class test {
      public static void main (String[] args) throws Exception
         {
-            ArrayList<Client> c = HibernateMethode.consultClientPgrm();
-            Map<String,Client> mc= new TreeMap<String, Client>(
-                new Comparator<String>() {
-                    public int compare(String obj1, String obj2) {
-                        // 降序排序
-                        return obj1.compareTo(obj2);
-                    }
-                });
-            for(Client cli : c){
-                mc.put(cli.getNomc().toString(), cli);
-            }
-         for (Map.Entry<String, Client> cc: mc.entrySet()) {
-             System.out.println(cc.getKey());
-         }
-
+            Exerciseperso ep = HibernateMethode.showExe(21);
+                String photo;
+                if(ep.getPhotoexe().equals("")){
+                    photo = "Bon Courage";
+                }else{
+                    photo = ep.getPhotoexe();
+                }
+                System.out.println("<photoexe>"+photo+"</photoexe>");
 
 }
 }
